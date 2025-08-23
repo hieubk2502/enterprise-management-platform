304b172fdb0433c719269fbd58fee3ae59e8e791
hieubk2502 <diemdungodau2502@gmail.com>
2025-08-23 10:03:19 +0700

    Init project

---
+++ vn-tmh-emp/emp/prc/src/main/java/vn/prc/PrcApplication.java
@@ -0,0 +1,13 @@
+package vn.prc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrcApplication.class, args);
+	}
+
+}
---
+++ vn-tmh-emp/emp/prc/src/test/java/vn/prc/PrcApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.prc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
